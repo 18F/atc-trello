b68785aef59378461253389aa33763762c558ca6
Greg Walker <michael.walker@gsa.gov>
2016-03-21 11:54:15 -0500

    Refactor webhook server into a separate file

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 require('dotenv').config();
-const Trello = require('node-trello');
 const bpa = require('bpa-trello-dashboard/app');
 const PORT = process.env.PORT || 5000;
-const t = new Trello(process.env.TRELLO_API_KEY, process.env.TRELLO_API_TOK);
+const WebhookServer = require('./webhookServer');
 const util = require('./util');
 const Logger = require('@erdc-itl/simple-logger');
 Logger.setOptions({
@@ -13,26 +12,27 @@ Logger.setOptions({
 });
 const log = new (require('@erdc-itl/simple-logger'))('main');
 
-var crypto = require('crypto');
+const trelloWHServer = new WebhookServer(PORT);
+trelloWHServer.start();
 
-function verifyTrelloWebhookRequest(body, signature) {
-  // Double-HMAC to blind any timing channel attacks
-  // https://www.isecpartners.com/blog/2011/february/double-hmac-verification.asp
-  var base64Digest = function (s) {
-    return crypto.createHmac('sha1', process.env.TRELLO_CLIENT_SECRET).update(s).digest('base64');
-  }
-  var content = body + __HOSTNAME;
-  var doubleHash = base64Digest(base64Digest(content));
-  var headerHash = base64Digest(signature);
-  return doubleHash == headerHash;
+trelloWHServer.on('data', e => {
+  console.log(e);
+});
+
+function cleanup() {
+  trelloWHServer.cleanup().then(() => process.exit(0));
 }
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
+process.on('exit', () => { });
 
 //const stageManager = new bpa.StageManager('runtime/bpa-stages.yaml', process.env.TRELLO_BPA_BOARD_ID);
 //stageManager.run();
 
 //const cardCreator = new bpa.CardCreator(null, process.env.TRELLO_BPA_BOARD_ID);
 
-/* */
+/* * /
 const http = require('http');
 const webhookListener = http.createServer((req, res) => {
   if(req.method.toLowerCase() === 'post' || req.method.toLowerCase() === 'put') {
@@ -84,7 +84,7 @@ const webhookListener = http.createServer((req, res) => {
                 });
               });
             });
-            //*/
+            //* /
           }
         }
       }
@@ -94,68 +94,5 @@ const webhookListener = http.createServer((req, res) => {
   } else {
     res.end();
   }
-});
-
-var __HOSTNAME;
-var __WEBHOOK_ID;
-
-webhookListener.listen(PORT, '0.0.0.0', () => {
-  log.info(`Listening on local port ${PORT}`);
-  util.getHostname(PORT).then(hostname => {
-    log.info(`Now accessible at ${hostname}`);
-    __HOSTNAME = hostname;
-    t.post('/1/webhooks', {
-      description: 'ATC Trello Webhook',
-      callbackURL: hostname,
-      idModel: process.env.ATC_TRELLO_BOARD_ID
-    }, function(err, data) {
-      if(err) {
-        log.error('Error setting up Trello webhook');
-        log.error(err);
-        process.exit(20);
-        return;
-      }
-
-      __WEBHOOK_ID = data.id;
-      log.info(`Trello webhook registered [${data.id}]`);
-    });
-  }).catch(e => {
-    log.error('Error getting hostname:');
-    log.error(e);
-    process.exit(10);
-  });
-});
-
-function cleanup() {
-  process.stdout.write('\r');
-  log.info('Shutting down');
-  t.del(`/1/webhooks/${__WEBHOOK_ID}`, function(err, data) {
-    if(err) {
-      log.error('Error unregistering Trello webhook');
-      log.error(err);
-    } else {
-      log.info('Trello webhook unregistered');
-    }
-    process.exit(0);
-  });
-}
-
-process.on('SIGINT', cleanup);
-process.on('SIGTERM', cleanup);
-process.on('exit', () => {
-});
-
-//*/
-
-/* * /
-// webhook ID
-t.post('/1/webhooks', {
-  description: 'ATC Trello Webhook',
-  callbackURL: 'http://darkcooger.ddns.net:9573',
-  idModel: process.env.TRELLO_BOARD_ID
-}, function(err, data) {
-  console.log(`Err: ${err}`);
-  console.log(err);
-  console.log(data);
 });
 //*/
---
+++ webhookServer.js
@@ -0,0 +1,116 @@
+"use strict";
+
+const Trello = require('node-trello');
+const http = require('http');
+const log = new (require('@erdc-itl/simple-logger'))('webhook server');
+const crypto = require('crypto');
+const util = require('./util');
+
+function verifyTrelloWebhookRequest(hostname, body, signature) {
+  // Double-HMAC to blind any timing channel attacks
+  // https://www.isecpartners.com/blog/2011/february/double-hmac-verification.asp
+  var base64Digest = function (s) {
+    return crypto.createHmac('sha1', process.env.TRELLO_CLIENT_SECRET).update(s).digest('base64');
+  }
+  var content = body + hostname;
+  var doubleHash = base64Digest(base64Digest(content));
+  var headerHash = base64Digest(signature);
+  return doubleHash == headerHash;
+}
+
+class TrelloWebhookServer {
+  constructor(port) {
+    this._port = port;
+    this._handlers = { data: [ ] };
+    this._trello = new Trello(process.env.TRELLO_API_KEY, process.env.TRELLO_API_TOK);
+  }
+
+  cleanup() {
+    return new Promise(resolve => {
+      log.info('Shutting down');
+      if(this._webhookID) {
+        this._trello.del(`/1/webhooks/${this._webhookID}`, function(err, data) {
+          if(err) {
+            log.error('Error unregistering Trello webhook');
+            log.error(err);
+          } else {
+            log.info('Trello webhook unregistered');
+          }
+          resolve();
+        });
+      }
+      else {
+        resolve();
+      }
+    });
+  }
+
+  start() {
+    this._server = http.createServer((req, res) => {
+      if(req.method.toLowerCase() === 'head') {
+        res.statusCode = 200;
+        res.end();
+      }
+      else if(req.method.toLowerCase() === 'post' || req.method.toLowerCase() === 'put') {
+        let trelloEvent = '';
+        log.verbose('Got a webhook event');
+
+        req.on('data', chunk => trelloEvent += chunk);
+        req.on('end', () => {
+          if(!verifyTrelloWebhookRequest(this._hostname, trelloEvent, req.headers['x-trello-webhook'] || '')) {
+            log.error('TRELLO SIGNATURE VERIFICATION FAILED');
+            res.statusCode = 400;
+            res.end();
+            return;
+          }
+          log.verbose('Valid Trello webhook event');
+
+          trelloEvent = JSON.parse(trelloEvent);
+          for(const handler of this._handlers.data) {
+            handler(trelloEvent);
+          }
+        });
+      }
+    });
+
+    this._server.listen(this._port, '0.0.0.0', () => {
+      log.info(`Listening on local port ${this._port}`);
+      util.getHostname(this._port).then(hostname => {
+        log.info(`Now accessible at ${hostname}`);
+        this._hostname = hostname;
+        this._trello.post('/1/webhooks', {
+          description: 'ATC Trello Webhook',
+          callbackURL: hostname,
+          idModel: process.env.ATC_TRELLO_BOARD_ID
+        }, function(err, data) {
+          if(err) {
+            log.error('Error setting up Trello webhook');
+            log.error(err);
+            process.exit(20);
+            return;
+          }
+
+          this._webhookID = data.id;
+          log.info(`Trello webhook registered [${data.id}]`);
+        }.bind(this));
+      }).catch(e => {
+        log.error('Error getting hostname:');
+        log.error(e);
+        process.exit(10);
+      });
+
+    })
+
+    return this;
+  }
+
+  on(eventName, handler) {
+    if(this._handlers[eventName] && typeof handler === 'function') {
+      this._handlers[eventName].push(handler)
+    }
+
+    return this;
+  }
+}
+
+module.exports = TrelloWebhookServer;
