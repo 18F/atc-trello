09e1df06a948ad3071ec2f60f6594d692a42e6a8
Greg Walker <michael.walker@gsa.gov>
2016-03-28 18:10:24 -0500

    Create BPA cards when a BPA project goes into In Flight

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('dotenv').config();
+const Trello = require('node-trello');
 const bpa = require('bpa-trello-dashboard/app');
 const PORT = process.env.PORT || 5000;
 const WebhookServer = require('./webhookServer');
@@ -8,16 +9,46 @@ const util = require('./util');
 const log = util.getLogger('main');
 
 const trelloWHServer = new WebhookServer(PORT);
-//trelloWHServer.start();
 
 function isMoveCardAction(action) {
   return (action.type === 'updateCard' && action.data.listAfter && action.data.listBefore);
+}
+
+const bpaCardCreator = new bpa.CardCreator(null, process.env.BPA_TRELLO_BOARD_ID);
+
+function addBpaCardsForAtc(cardID) {
+  let trello = new Trello(process.env.TRELLO_API_KEY, process.env.TRELLO_API_TOK);
+  trello.get(`/1/cards/${cardID}`, (err, card) => {
+    if(err) {
+      log.error('Error getting Trello card');
+      log.error(err);
+      return;
+    }
+
+    // If one of the "BPA: " lines of the description already
+    // has a link, don't need to create another card.
+    const bpasNeeded = card.desc.match(/BPA:([^\n]+)/gi).filter(bpa => bpa.indexOf("https://trello.com") < 0);
+    for(const bpa of bpasNeeded) {
+      bpaCardCreator.createCard({
+        project: card.name,
+        order: bpa.substr(4).trim(),
+        trello: card.shortUrl
+      }).then(bpaCard => {
+        card.desc = card.desc.replace(bpa, `BPA: ${bpaCard.url}`);
+        trello.put(`/1/card/${card.id}`, card, () => { });
+      }).catch(err => {
+        log.error('Error creating BPA card');
+        log.error(err);
+      });
+    }
+  });
 }
 
 trelloWHServer.start().then(s => s.on('data', e => {
   if(isMoveCardAction(e.action)) {
     if(e.action.data.listAfter.name === 'In Flight') {
       log.verbose('Card was moved to In Flight');
+      addBpaCardsForAtc(e.action.data.card.id);
     }
   }
 })).catch(e => {
@@ -33,73 +64,3 @@ function cleanup() {
 process.on('SIGINT', cleanup);
 process.on('SIGTERM', cleanup);
 process.on('exit', () => { });
-
-//const stageManager = new bpa.StageManager('runtime/bpa-stages.yaml', process.env.TRELLO_BPA_BOARD_ID);
-//stageManager.run();
-
-//const cardCreator = new bpa.CardCreator(null, process.env.TRELLO_BPA_BOARD_ID);
-
-/* * /
-const http = require('http');
-const webhookListener = http.createServer((req, res) => {
-  if(req.method.toLowerCase() === 'post' || req.method.toLowerCase() === 'put') {
-    let trelloEvent = '';
-    log.verbose('Got a webhook event');
-
-    req.on('data', chunk => trelloEvent += chunk);
-    req.on('end', () => {
-      if(!verifyTrelloWebhookRequest(trelloEvent, req.headers['x-trello-webhook'])) {
-        log.error('TRELLO SIGNATURE VERIFICATION FAILED');
-        return;
-      }
-      log.verbose('Valid Trello webhook event');
-
-      trelloEvent = JSON.parse(trelloEvent);
-
-      if(trelloEvent.action.type === 'updateCard') {
-        if(trelloEvent.action.data.listAfter && trelloEvent.action.data.listBefore) {
-          // Card was moved
-          if(trelloEvent.action.data.listAfter.name === 'In Flight') {
-            console.log('Card was moved to In Flight');
-
-            /* * /
-            t.get(`/1/cards/${tEvent.action.data.card.id}`, (err, card) => {
-              const bpas = card.desc.match(/BPA:([^\n]+)/gi).filter(bpa => bpa.indexOf("https://trello.com") < 0);
-
-              const allBPACardPromises = [ ];
-              for(let bpa of bpas) {
-                allBPACardPromises.push(new Promise((bpaCardResolve, bpaCardReject) => {
-                  cardCreator.createCard({
-                    project: card.name,
-                    order: bpa.substr(4).trim()
-                  }).then(bpaCard => {
-                    card.desc = card.desc.replace(bpa, `${bpa} ${bpaCard.url}`);
-                    bpaCard.desc = bpaCard.desc.replace(`Project: ${card.name}`, `Project: ${card.url}`);
-                    t.put(`/1/card/${bpaCard.id}`, bpaCard, () => { });
-                    bpaCardResolve();
-                  }).catch(err => {
-                    console.log(err);
-                    bpaCardResolve();
-                  });
-                }));
-              }
-
-              Promise.all(allBPACardPromises).then(() => {
-                t.put(`/1/card/${card.id}`, card, (updateErr, data) => {
-                  console.log(updateErr);
-                  console.log(data);
-                });
-              });
-            });
-            //* /
-          }
-        }
-      }
-
-      res.end();
-    })
-  } else {
-    res.end();
-  }
-});
-//*/
