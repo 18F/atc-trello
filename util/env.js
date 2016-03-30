ff41c4214e2355a40a44ad7d2e00fc7c5b328f83
Greg Walker <michael.walker@gsa.gov>
2016-03-30 08:08:00 -0500

    • Add manifest • Move env var config into a separate file and have it account for CF with cfenv

---
+++ util/env.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('dotenv').config();
+var cfenv = require('cfenv');
+var appEnv = cfenv.getAppEnv();
+
+const knownEnvs = [
+  'TRELLO_API_KEY',
+  'TRELLO_CLIENT_SECRET',
+  'TRELLO_API_TOK',
+  'ATC_TRELLO_BOARD_ID',
+  'BPA_TRELLO_BOARD_ID',
+  'LOCALTUNNEL',
+  'PORT',
+  'HOST',
+  'LOG_LEVEL'
+]
+
+if (appEnv.getServices() && Object.keys(appEnv.getServices()).length) {
+  // If running on Cloud Foundry
+  for(const env of knownEnvs) {
+    process.env[env] = appEnv.getServiceCreds('atc-trello-cups')[env];
+  }
+}
