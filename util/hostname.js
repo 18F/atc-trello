5b04b2eb6b223a9b58eed189b7636db08a384770
Greg Walker <michael.walker@gsa.gov>
2016-04-20 11:51:51 -0500

    Remove internal webhook server, use @18f/trello-webhook-server instead

--- util/hostname.js
+++ util/hostname.js
@@ -4,8 +4,10 @@ const log = require('./logger')('get hostname');
 
 module.exports = function getHostname(port) {
   if (process.env.HOST) {
+    log.verbose(`Using env host (${process.env.HOST})`);
     return Promise.resolve(process.env.HOST);
   } else if (process.env.LOCALTUNNEL === 'true') {
+    log.verbose('Setting up localtunnel...');
     return new Promise((resolve, reject) => {
       const lt = require('localtunnel');
       lt(port, /* { host: 'https://localtunnel.18f.gov' },*/ (err, tunnel) => {
@@ -17,6 +19,7 @@ module.exports = function getHostname(port) {
           log.info('localtunnel has closed');
         });
 
+        log.verbose(`Got localtunnel host (${tunnel.url})`);
         return resolve(tunnel.url);
       });
     });
